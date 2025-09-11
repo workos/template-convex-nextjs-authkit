18494fa252ced03fab4c5a679c007addbae0dbcb
Nick Nisi <nick.nisi@workos.com>
2025-09-11 18:55:41 -0500

    Enable eagerAuth for immediate access token availability (#11)
    * optimize template to use eagerAuth feature
    
    * update to authkit-nextjs v2.7.0

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
 export default authkitMiddleware({
+  eagerAuth: true,
   middlewareAuth: {
     enabled: true,
     unauthenticatedPaths: ['/', '/sign-in', '/sign-up'],
